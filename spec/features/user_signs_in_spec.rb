98c0bfd9d4cc0adfe8235161a28ec28ac8579962
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-18 18:17:31 -0500

    changed top bar to include functional Sign in link.  Passes test

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -16,7 +16,7 @@ feature 'authenticated user may sign in', %Q(
 
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_button 'Sign in'
+    click_button 'Log in'
 
     expect(page).to have_content("Signed in successfully.")
   end
