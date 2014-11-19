1691311a35678db88c0dae73be8b79769cdd29c3
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-18 19:31:23 -0500

    added boolean for accepted to friendships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119001045) do
+ActiveRecord::Schema.define(version: 20141119002847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141119001045) do
   create_table "friendships", force: true do |t|
     t.integer "user_id",     null: false
     t.integer "friendee_id", null: false
+    t.boolean "accepted"
   end
 
   create_table "users", force: true do |t|
