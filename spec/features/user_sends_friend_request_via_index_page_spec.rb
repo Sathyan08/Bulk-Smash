4bcf9f5d4a9fe225924a1359fe74d7744f342ead
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-19 16:32:06 -0500

    added placeholder show page and made index page have links to each individual show page

--- spec/features/user_sends_friend_request_via_index_page_spec.rb
+++ spec/features/user_sends_friend_request_via_index_page_spec.rb
@@ -1,1 +1,26 @@
-require 'rails helper'
+require 'rails_helper'
+
+feature 'user can find friends via the index page and then submit a friend request', %Q(
+
+  As a user of Bulk Smash,
+  I want to be able to find my friends
+  so that I can
+
+) do
+
+  scenario 'user signs in' do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link 'Sign in'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    20.times { FactoryGirl.create(:user) }
+
+    save_and_open_page
+  end
+
+end
