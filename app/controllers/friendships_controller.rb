fb119d2a0910cfbfa33d523f72ef25539cf2d4fd
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-19 19:37:27 -0500

    added method logic for accepting friend requests to the friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,7 +12,14 @@ class FriendshipsController < ApplicationController
   end
 
   def accept_request
-    binding.pry
+    @request = Friendship.find_or_initialize_by(user_id: params[:id] friendee_id: current_user.id)
+    @request.accepted = true
+
+    if @request.save
+      redirect_to User.find(params[:id]), notice: "Friend Request Successfully Accepted."
+    else
+      redirect_to User.find(params[:id]), notice: "Friend Request Not Accepted!"
+    end
   end
 
 end
