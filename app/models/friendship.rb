24ea731d187cc1ac24fabd710991860e0e779755
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-25 23:48:55 -0500

    added scope to only retrieve accepted friend requests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ActiveRecord::Base
+
+  scope :confirmed, -> { where(accepted: true) }
+
   belongs_to :user
   belongs_to :friendee, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,22 @@ class User < ActiveRecord::Base
   has_many :friendships_received, class_name: "Friendship", foreign_key: :friendee_id
   has_many :frienders, through: :friendships_received, source: :user
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def accepted_friendships
-    friendships_requested.where("accepted = true") + friendships_received("accepted = true")
+    friendships_requested.confirmed + friendships_received.confirmed
   end
 
   def friends
     friends_array = []
 
     accepted_friendships.each do |friendship|
-      if friendship.user != self
+      if friendship.user == self
+        friends_array << friendship.friendee
+      else
         friends_array << friendship.user
       end
     end
