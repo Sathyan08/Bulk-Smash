ab77c6a0a9bf9303f0b3a27c17d49ecd2342d253
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-26 22:56:33 -0500

    Added uniqueness validation of friend with scope of user to friendship model.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,6 +6,8 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friendee, class_name: "User"
 
+  validates_uniqueness_of :friendee, scope: :user
+
   def find_friend_of(target_user)
     (user == target_user)? friendee : user
   end
