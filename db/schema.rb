2f3a95c37b35dd4ca44b4b94ef7440736dcb3890
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-27 17:29:50 -0500

    renamed entries table to list_items with migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127221453) do
+ActiveRecord::Schema.define(version: 20141127222713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "entries", force: true do |t|
-    t.integer  "list_id",    null: false
-    t.integer  "food_id",    null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "foods", force: true do |t|
     t.string   "name",       null: false
@@ -33,6 +26,13 @@ ActiveRecord::Schema.define(version: 20141127221453) do
     t.integer "user_id",     null: false
     t.integer "friendee_id", null: false
     t.boolean "accepted"
+  end
+
+  create_table "list_items", force: true do |t|
+    t.integer  "list_id",    null: false
+    t.integer  "food_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "lists", force: true do |t|
