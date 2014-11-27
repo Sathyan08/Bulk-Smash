b20d50bff73360479e0f04b4c2c2cb1cb99b87a7
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-27 17:24:16 -0500

    added shoulda tests for lists and entries to user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,5 +13,7 @@ describe User do
   it { should have_many(:friendships_received) }
   it { should have_many(:friendees) }
   it { should have_many(:frienders) }
+  it { should have_many(:lists) }
+  it { should have_many(:entries) }
 
 end
