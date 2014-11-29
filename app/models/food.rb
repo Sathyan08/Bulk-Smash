4f31a39b7d0d6615006b73a8e97ba1d83a5704cd
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-28 20:33:22 -0500

    added validates uniqueness of name to food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,5 +3,6 @@ class Food < ActiveRecord::Base
   belongs_to :unit
 
   validates :name, presence: true
+  validates_uniqueness_of :name
 
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,5 +3,6 @@ class Unit < ActiveRecord::Base
   has_many :foods
 
   validates :name, presence: true
+  validates_uniqueness_of :name
 
 end
