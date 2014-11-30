39c2463d0d9bab6ec6c0ce0a7c21a0f2b3017099
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-29 19:56:53 -0500

    Added columns for name and current to lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128000844) do
+ActiveRecord::Schema.define(version: 20141130005343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 20141128000844) do
   end
 
   create_table "lists", force: true do |t|
-    t.integer  "user_id",    null: false
+    t.integer  "user_id",                   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "current",    default: true
+    t.string   "name",                      null: false
   end
 
   create_table "units", force: true do |t|
