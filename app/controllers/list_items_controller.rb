78ac862d97ad3248f38202a6d7996b368f878c3f
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-12-04 18:40:28 -0500

    revised list_items controller and list show page to both allow for the proper creation of list items and the proper display of that information.

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,11 +1,24 @@
-class ListsItemsController < ApplicationController
+class ListItemsController < ApplicationController
 
   def new
     @list_item = ListItem.new
   end
 
   def create
-    binding.pry
+    @list_item = ListItem.new(list_item_params)
+    list = List.find(params[:list_id])
+    @list_item.list = list
+
+    if @list_item.save
+      redirect_to list_path(list), notice: "Successfully Created"
+    else
+      redirect_to list_path(list), notice: "Could Not Save"
+    end
   end
 
+  private
+
+  def list_item_params
+    params.require(:list_item).permit(:list_id, :food_id, :amount)
+  end
 end
