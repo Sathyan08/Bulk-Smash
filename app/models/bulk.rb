461ac3e6aaaa068607c3bda9cb33801ebd93e6cd
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-12-26 15:09:42 -0500

    Added validations for food_id to the bulk model

--- app/models/bulk.rb
+++ app/models/bulk.rb
@@ -2,6 +2,7 @@ class Bulk < ActiveRecord::Base
 
   belongs_to :food
 
+  validates :food_id, numericality: true, presence: true
   validates :amount, numericality: { greater_than: 0 }, presence: true
   validates :price, numericality: { greater_than: 0 }, presence: true
 
