2752e2053d8e193db0c1b5fec999e7ed37c4e7d7
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-12-26 14:38:40 -0500

    changed numericality test to only accept values greater than 0

--- app/models/bulk.rb
+++ app/models/bulk.rb
@@ -2,7 +2,7 @@ class Bulk < ActiveRecord::Base
 
   belongs_to :food
 
-  validates :amount, numericality: true, presence: true
-  validates :price, numericality: true, presence: true
+  validates :amount, numericality: { greater_than: 0 }, presence: true
+  validates :price, numericality: { greater_than: 0 }, presence: true
 
 end
