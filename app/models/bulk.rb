7f229f9a806ef7bcbf9d6be6655af11ff3906da3
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-12-26 14:33:36 -0500

    added numericality validations to the bulks model for the amount and price validations

--- app/models/bulk.rb
+++ app/models/bulk.rb
@@ -2,7 +2,7 @@ class Bulk < ActiveRecord::Base
 
   belongs_to :food
 
-  validates :amount, presence: true
-  validates :price, presence: true
+  validates :amount, numericality: true, presence: true
+  validates :price, numericality: true, presence: true
 
 end
